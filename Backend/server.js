6db676a8a5c63f82fb0cb46c1bbf1074df01a79d
khxleeq <73849637+khxleeq@users.noreply.github.com>
2022-06-28 10:47:27 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const colours = require('colors')
 const dovenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
@@ -12,6 +13,8 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+app.use(errorHandler)
 
 app.use('/api/receipt', require('./routes/receiptRoute'));
 
